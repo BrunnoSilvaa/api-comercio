3716c79c19a9167a4e9a7f1820bd26f1574a9bdd
BrunnoSilvaa <brunnosilvaraujo@gmail.com>
2022-10-24 12:33:58 -0300

    buscar por id

--- exercico/src/main/java/com/api/exercico/services/ProdutoService.java
+++ exercico/src/main/java/com/api/exercico/services/ProdutoService.java
@@ -1,7 +1,7 @@
 package com.api.exercico.services;
 
 import java.util.List;
-
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.api.exercico.DTO.ProdutoDTO;
 import com.api.exercico.domain.Produto;
 import com.api.exercico.repositories.ProdutoRepository;
+import com.api.exercico.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ProdutoService {
@@ -20,8 +21,16 @@ public class ProdutoService {
 	
 	@Transactional(readOnly = true)
 	public List<ProdutoDTO> findAll(){
-		
 		List<Produto> list = repository.findAll();
 		return list.stream().map(x -> new ProdutoDTO(x)).collect(Collectors.toList());
+	}
+
+	
+	@Transactional(readOnly = true)
+	public ProdutoDTO findById(Long id) {
+		
+		Optional<Produto> obj = repository.findById(id);
+		Produto entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity Not Fund"));
+		return new ProdutoDTO(entity);
 	}
 }
